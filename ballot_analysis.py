043ad9fd707ce3097740c0c39b7b7b0bdb353232
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-01-09 16:24:10 -0700

    Update ballot_analysis.py

--- ballot_analysis.py
+++ ballot_analysis.py
@@ -160,12 +160,12 @@ with st.sidebar:
     pad1, col1, pad2 = st.columns([0.2,0.6,0.2])
     with col1:
         st.image(letter_logo)
-    voter_list = tracker_years['Voter'].sort_values().unique()
+    voter_list = tracker_years.loc[tracker_years['year']==ss['year'],'Voter'].sort_values().unique()
     st.selectbox('Select a voter',
                  voter_list,
                  key='voter')
   
-    years_list = tracker_years.loc[tracker_years['Voter']==ss['voter'],'year'].sort_values(ascending=False).unique()
+    years_list = tracker_years['year'].sort_values(ascending=False).unique()
     st.selectbox('Select a ballot year',
                  years_list,
                  key='year')
