9d8009bd0181cf508124c64519dc756b4965846d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-01-10 00:03:21 -0700

    Update ballot_analysis.py

--- ballot_analysis.py
+++ ballot_analysis.py
@@ -200,9 +200,9 @@ def population_change():
     del ss['voter']
 
 with st.sidebar:
-    pad1, col1, pad2 = st.columns([0.3,0.4,0.3])
-    with col1:
-        st.image(letter_logo)
+    # pad1, col1, pad2 = st.columns([0.3,0.4,0.3])
+    # with col1:
+    #     st.image(letter_logo)
     if ss['year']==2026:
         pl_tracker_years = load_pl_data()
         pl_staff = st.checkbox("PL Staff?",value=False,
