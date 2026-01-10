006e2506ddc34fbf9f4619946dd68432c25d974a
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-01-10 01:15:41 -0700

    Update ballot_analysis.py

--- ballot_analysis.py
+++ ballot_analysis.py
@@ -259,10 +259,10 @@ def ballot_chart(voter, year,pl_staff=pl_staff):
                 ax=ax1,
                 legend=False)
     ax1.axvline(0,color=pl_line_color)
-    ax1.axhline(-0.7,color=pl_line_color,xmin=1/4,xmax=3/4,linewidth=2)
+    ax1.axhline(-0.7,color=pl_line_color,xmin=1/8,xmax=7/8,linewidth=2)
     player_votes = voter_df['Total Votes'].item()
-    ax1.text(1,-1,f'Voted For ({player_votes:.0f})',ha='right',color=pl_line_color)
-    ax1.text(-1,-1,'Did Not Vote For',ha='left',color=pl_line_color)
+    ax1.text(1.5,-1,f'Voted For ({player_votes:.0f})',ha='right',color=pl_line_color)
+    ax1.text(-1.5,-1,'Did Not Vote For',ha='left',color=pl_line_color)
     for player in ax1.get_yticklabels():
         vaa = chart_df.loc[chart_df['Player']==player.get_text(),'Votes Above Average'].item()
         ax1.text(vaa + (0.02 if vaa >=0 else -0.02),
